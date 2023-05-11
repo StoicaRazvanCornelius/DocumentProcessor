3a669253840ec981fa33aa9f7e52eded4a7cb8c2
RAZVAN STOICA <razvan.stoica18@gmail.com>
2023-05-11 23:08:42 +0300

    -added partial gui for import. Needs to connect with xls and other types of files

--- src/main/java/ro/ti/documentProcessor/views/ImportPresenter.java
+++ src/main/java/ro/ti/documentProcessor/views/ImportPresenter.java
@@ -7,8 +7,11 @@ import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import eu.hansolo.medusa.Clock;
 import eu.hansolo.medusa.ClockBuilder;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
@@ -16,6 +19,10 @@ public class ImportPresenter {
 
     @FXML
     private View importView;
+
+    @FXML
+    private Button openFileBtn;
+
 
     public void initialize() {
         importView.setShowTransitionFactory(BounceInRightTransition::new);
@@ -29,5 +36,14 @@ public class ImportPresenter {
             }
         });
     }
-    
+
+    public void openFile(ActionEvent actionEvent){
+        openFileBtn.setText("asd");
+    }
+
+    public void clickSound(MouseEvent mouseEvent) {
+    }
+
+    public void saveFile(ActionEvent actionEvent) {
+    }
 }
